4934d1f09b449aba05688a77c0ef22deb447ede0
Petr Kouba <petrkouba3@gmail.com>
2024-10-30 12:43:47 +0200

    script to prepare the chain_set.jsonl files

--- data/scripts/prepare_dataset.py
+++ data/scripts/prepare_dataset.py
@@ -3,22 +3,24 @@ import os
 import re
 import json
 import yaml
+from tqdm import tqdm
 
-fold_dirs = yaml.load(open('configs/data_config.yaml'))['pdb_dir']
+in_dir = yaml.load(open('configs/data_config.yaml', 'r'), Loader=yaml.FullLoader)['pdb_dir']
+out_dir = yaml.load(open('configs/data_config.yaml', 'r'), Loader=yaml.FullLoader)['preprocessed_dir']
 
 fold_list = []
-fold_files = os.listdir(fold_dir)
+fold_files = os.listdir(in_dir)
 fold_files = [filename for filename in fold_files if re.match(".*\.pdb$", filename)]
 
-for file in fold_files:
+for file in tqdm(fold_files):
     _name= file.split('_')[0]
     _chain = file.split('_')[1]
-    _path = f'{fold_dir}/{file}'
+    _path = f'{in_dir}/{file}'
     old_pdb = parse_PDB(_path,name=_name, input_chain_list=[_chain])[0]
     new_pdb = align_pdb_dict_formats(old_pdb,_chain)
     fold_list.append(new_pdb)
 
-with open(f'{fold_dir}/chain_set.jsonl','w') as f:
+with open(f'{out_dir}/chain_set.jsonl','w') as f:
     for dict in fold_list:
         json.dump(dict,f)
         f.write('\n')
