56b48c1ae2656426c2fe13ae45ed93cdad797346
Petr Kouba <petrkouba3@gmail.com>
2024-11-15 12:34:13 +0200

    added collator utils for ESM

--- utils/utils.py
+++ utils/utils.py
@@ -232,3 +232,105 @@ def compute_metrics(eval_pred):
     return {"spearmanr": spearman.compute(predictions=valid_predictions, references=valid_labels)['spearmanr'],
             "pearsonr": pearson.compute(predictions=valid_predictions, references=valid_labels)['pearsonr'],
             "mse": mse.compute(predictions=valid_predictions, references=valid_labels)['mse']}
+
+### ESM-2
+
+# based on transformers DataCollatorForTokenClassification
+@dataclass
+class DataCollatorForTokenRegression_esm(DataCollatorMixin):
+    """
+    Data collator that will dynamically pad the inputs received, as well as the labels.
+    Args:
+        tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
+            The tokenizer used for encoding the data.
+        padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `True`):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            - `True` or `'longest'` (default): Pad to the longest sequence in the batch (or no padding if only a single
+              sequence is provided).
+            - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
+              acceptable input length for the model if that argument is not provided.
+            - `False` or `'do_not_pad'`: No padding (i.e., can output a batch with sequences of different lengths).
+        max_length (`int`, *optional*):
+            Maximum length of the returned list and optionally padding length (see above).
+        pad_to_multiple_of (`int`, *optional*):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+        label_pad_token_id (`int`, *optional*, defaults to -100):
+            The id to use when padding the labels (-100 will be automatically ignore by PyTorch loss functions).
+        return_tensors (`str`):
+            The type of Tensor to return. Allowable values are "np", "pt" and "tf".
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    label_pad_token_id: int = -100
+    return_tensors: str = "pt"
+
+    def torch_call(self, features):
+        import torch
+
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature[label_name] for feature in features] if label_name in features[0].keys() else None
+
+        no_labels_features = [{k: v for k, v in feature.items() if k != label_name} for feature in features]
+
+        batch = self.tokenizer.pad(
+            no_labels_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        if labels is None:
+            return batch
+
+        sequence_length = batch["input_ids"].shape[1]
+        padding_side = self.tokenizer.padding_side
+
+        def to_list(tensor_or_iterable):
+            if isinstance(tensor_or_iterable, torch.Tensor):
+                return tensor_or_iterable.tolist()
+            return list(tensor_or_iterable)
+
+        if padding_side == "right": 
+            # changed to pad the special tokens at the beginning and end of the sequence
+            batch[label_name] = [
+                [self.label_pad_token_id] + to_list(label) + [self.label_pad_token_id] * (sequence_length - len(label)-1) for label in labels
+                ]                
+        else:
+            batch[label_name] = [
+                [self.label_pad_token_id] * (sequence_length - len(label)) + to_list(label) for label in labels
+                ]
+        # change datatype from torch.int64 to torch.float
+        batch[label_name] = torch.tensor(batch[label_name], dtype=torch.float)
+        return batch
+
+#load ESM2 models
+def load_esm_model_regression(checkpoint, half_precision, full = False, deepspeed=True):
+    
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    
+    if half_precision and deepspeed:
+        model = EsmForTokenRegression.from_pretrained(checkpoint, num_labels = 1, torch_dtype = torch.float16)
+    else:
+        model = EsmForTokenRegression.from_pretrained(checkpoint, num_labels = 1)
+        
+    if full == True:
+        return model, tokenizer 
+        
+    peft_config = LoraConfig(
+        r=4, lora_alpha=1, bias="all", target_modules=["query","key","value","dense"]
+    )
+    
+    model = inject_adapter_in_model(peft_config, model) 
+    
+    # Unfreeze the prediction head
+    for (param_name, param) in model.classifier.named_parameters():
+                param.requires_grad = True 
+    
+    return model, tokenizer
