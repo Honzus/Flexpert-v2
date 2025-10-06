310492d2ae7c1df1c28d5917c013db4e79b40fda
Jan Richtr <richtrhonzi@gmail.com>
2025-10-06 14:36:41 +0200

    Implemented the ESM2-150M model

--- models/T5_encoder_per_token.py
+++ models/T5_encoder_per_token.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import copy
 import re
-from transformers import T5Config, T5PreTrainedModel, T5EncoderModel, T5Tokenizer
+from transformers import T5Config, T5PreTrainedModel, T5EncoderModel, T5Tokenizer, AutoModel, AutoTokenizer, EsmConfig, EsmModel, EsmPreTrainedModel
 from transformers.models.t5.modeling_t5 import T5Stack
 from transformers.modeling_outputs import TokenClassifierOutput
 from transformers.utils.model_parallel_utils import assert_device_map, get_device_map
@@ -168,5 +168,158 @@ def PT5_classification_model(half_precision, class_config):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
     print("ProtT5_LoRA_Classfier\nTrainable Parameter: "+ str(params) + "\n")
+    
+    return model, tokenizer
+
+class ESM2EncoderForTokenClassification(EsmPreTrainedModel):
+
+    def __init__(self, config: EsmConfig, class_config):
+        super().__init__(config)
+        self.num_labels = class_config.num_labels
+        self.config = config
+        self.add_pearson_loss = class_config.add_pearson_loss
+        self.add_sse_loss = class_config.add_sse_loss
+
+        encoder_config = copy.deepcopy(config)
+        encoder_config.use_cache = False
+        encoder_config.is_encoder_decoder = False
+        self.encoder = EsmModel(config)
+
+        self.dropout = nn.Dropout(class_config.dropout_rate)
+        if class_config.adaptor_architecture == 'attention':
+            self.classifier = ENMAdaptedAttentionClassifier(config.hidden_size, class_config.num_labels, class_config.enm_embed_dim, class_config.enm_att_heads) #nn.Linear(config.hidden_size, class_config.num_labels)
+        elif class_config.adaptor_architecture == 'direct':
+            self.classifier = ENMAdaptedDirectClassifier(config.hidden_size, class_config.num_labels)
+        elif class_config.adaptor_architecture == 'conv':
+            self.classifier = ENMAdaptedConvClassifier(config.hidden_size, class_config.num_labels, class_config.kernel_size, class_config.enm_embed_dim, class_config.num_layers)
+        elif class_config.adaptor_architecture == 'no-adaptor':
+            self.classifier = ENMNoAdaptorClassifier(config.hidden_size, class_config.num_labels)
+        else:
+            raise ValueError('Only attention, direct, conv and no-adaptor architectures are supported for the adaptor.')
+
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+        # Model parallel
+        self.model_parallel = False
+        self.device_map = None
+
+    def parallelize(self, device_map=None):
+        self.device_map = (
+            get_device_map(len(self.encoder.block), range(torch.cuda.device_count()))
+            if device_map is None
+            else device_map
+        )
+        assert_device_map(self.device_map, len(self.encoder.block))
+        self.encoder.parallelize(self.device_map)
+        self.classifier = self.classifier.to(self.encoder.first_device)
+        self.model_parallel = True
+
+    def deparallelize(self):
+        self.encoder.deparallelize()
+        self.encoder = self.encoder.to("cpu")
+        self.model_parallel = False
+        self.device_map = None
+        torch.cuda.empty_cache()
+
+    def get_input_embeddings(self):
+        return self.encoder.embeddings.word_embeddings
+
+    def set_input_embeddings(self, new_embeddings):
+        self.encoder.embeddings.word_embeddings = new_embeddings
+
+    def get_encoder(self):
+        return self.encoder
+
+    def _prune_heads(self, heads_to_prune):
+        """
+        Prunes heads of the model. heads_to_prune: dict of {layer_num: list of heads to prune in this layer} See base
+        class PreTrainedModel
+        """
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    def forward(
+        self,
+        enm_vals = None,
+        input_ids=None,
+        attention_mask=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        # import pdb; pdb.set_trace()
+        outputs = self.encoder(input_ids=input_ids,
+            attention_mask=attention_mask,
+            inputs_embeds=inputs_embeds,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        #TODO: check the enm_vals are padded properly and check that the sequence limit (in the transformer) is indeed 512
+        logits = self.classifier(sequence_output, enm_vals, attention_mask)
+        
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return TokenClassifierOutput(
+            #loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+def ESM2_classification_model(half_precision, class_config):
+    if not half_precision:
+        model = AutoModel.from_pretrained("facebook/esm2_t30_150M_UR50D", local_files_only=True)
+        tokenizer = AutoTokenizer.from_pretrained('facebook/esm2_t30_150M_UR50D', local_files_only=True)
+    elif half_precision and torch.cuda.is_available(): 
+        tokenizer = AutoTokenizer.from_pretrained('facebook/esm2_t30_150M_UR50D', do_lower_case=False, local_files_only=True)
+        model = AutoModel.from_pretrained("facebook/esm2_t30_150M_UR50D", torch_dtype=torch.float16, local_files_only=True).to(torch.device('cuda'))
+    else:
+        raise ValueError('Half precision can be run on GPU only.')
+    
+    # Create new Classifier model with PT5 dimensions
+    class_model=ESM2EncoderForTokenClassification(model.config,class_config)
+    
+    # Set encoder and embedding weights to checkpoint weights
+    class_model.encoder=model  
+    #import pdb; pdb.set_trace()
+    # Delete the checkpoint model
+    model = class_model
+    
+    # Print number of trainable parameters
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    print("ESM2-650M_Classfier\nTrainable Parameter: "+ str(params))    
+ 
+    # Add model modification lora
+    config = LoRAConfig('configs/lora_config.yaml')
+    
+    # Add LoRA layers
+    model = modify_with_lora(model, config)
+    
+    # Freeze Embeddings and Encoder (except LoRA)
+    for (param_name, param) in model.encoder.named_parameters():
+                param.requires_grad = False       
+
+    for (param_name, param) in model.named_parameters():
+            if re.fullmatch(config.trainable_param_names, param_name):
+                param.requires_grad = True
+
+    # Print trainable Parameter          
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    print("ESM2-650M_LoRA_Classfier\nTrainable Parameter: "+ str(params) + "\n")
     
     return model, tokenizer
